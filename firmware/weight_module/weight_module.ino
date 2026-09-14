3d257089935d2ca02f1d46b3d1bb84c924089758
Dmitriy <d.a.muravyev1@gmail.com>
2026-09-14 22:44:37 +0500

    initial firmware setup

---
+++ firmware/weight_module/weight_module.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int value = analogRead(A0);
+
+  if (value > 500) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+
+  Serial.println(value);
+  delay(1000);
+}
